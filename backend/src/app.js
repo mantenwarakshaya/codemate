43690c8ad549fd468aa76a33de7054b56f81190e
mantenwarakshaya <mantenwarakshaya05@gmail.com>
2026-04-12 12:04:27 +0530

    fix frontend dist path

--- backend/src/app.js
+++ backend/src/app.js
@@ -39,7 +39,6 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
 });
-
 // ✅ Server
 const PORT = process.env.PORT || 7777;
 
