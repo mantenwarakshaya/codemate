90a688375649fe66e1263ec4adb76ad8167e492a
mantenwarakshaya <mantenwarakshaya05@gmail.com>
2026-04-13 21:45:40 +0530

    Fixing file case sensitivity for deployment

--- frontend/src/components/Chat/index.jsx
+++ frontend/src/components/Chat/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { createSocketConnection } from "../utils/socket";
+import { createSocketConnection } from "../utils/socket.js";
 import { useSelector } from "react-redux";
 import axios from "axios";
 import "./index.css";
