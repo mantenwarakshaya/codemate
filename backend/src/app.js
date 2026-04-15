12df475faa534f8c768ab924904dc27f0973538b
mantenwarakshaya <mantenwarakshaya05@gmail.com>
2026-04-15 22:31:01 +0530

    removed old socket.io and added new code

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const cors = require("cors");
 const http = require("http");
 require("dotenv").config();
-
+process.env.FRONTEND_URL = "http://localhost:5173";
 // ✅ Middlewares
 app.use(express.json());
 app.use(cookieParser());
@@ -14,7 +14,8 @@ app.use(cookieParser());
 // ✅ CORS (simple since same domain)
 app.use(
   cors({
-    origin: "https://codemate-xd74.onrender.com",
+    // origin: "https://codemate-xd74.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
@@ -37,13 +38,15 @@ app.use("/api", chatRouter);
 const server = http.createServer(app);
 initializeSocket(server);
 
-// 🔥 Serve frontend (React build)
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+if (process.env.NODE_ENV === "production"){
+  // 🔥 Serve frontend (React build)
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-// ✅ Use a Regex Literal (no quotes) instead of a string
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
-});
+  // ✅ Use a Regex Literal (no quotes) instead of a string
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  });
+}
 // ✅ Server
 const PORT = process.env.PORT || 7777;
 
