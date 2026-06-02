aa13dcb582caa892a9a820bd13dd8539f79ddf49
mantenwarakshaya <mantenwarakshaya05@gmail.com>
2026-06-02 22:36:43 +0530

    fix: url

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,7 +26,7 @@ cloudinary.config({
 app.use(express.json());
 
 // for production, CORS is not needed since frontend and backend are on same domain
-app.use(cors());
+// app.use(cors());
 
 // for localhost development, allow CORS from frontend
 // app.use(
@@ -35,6 +35,16 @@ app.use(cors());
 //     credentials: true,
 //   })
 // );
+
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? process.env.FRONTEND_URL
+        : "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
