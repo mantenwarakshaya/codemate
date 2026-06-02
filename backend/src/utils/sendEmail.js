4637d651d205bef63d86c6fc3d2e586f414c2838
mantenwarakshaya <mantenwarakshaya05@gmail.com>
2026-06-02 20:40:22 +0530

    fix: fixed send email

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -4,6 +4,10 @@ const sendEmail = async (to, subject, html) => {
 
   const transporter = nodemailer.createTransport({
     service: "gmail",
+    host: "smtp.gmail.com", // 👈 Explicitly add the host
+    port: 465,              // 👈 Use the secure port matching your error log
+    secure: true,
+    localAddress: "0.0.0.0", // 👈 FORCE IPv4 ONLY (This bypasses Render's IPv6 bug)
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
