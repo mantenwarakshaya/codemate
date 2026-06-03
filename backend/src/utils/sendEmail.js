4e9f4d75ebd4d3b6ce563e296dc3e326e896553a
mantenwarakshaya <mantenwarakshaya05@gmail.com>
2026-06-03 17:38:06 +0530

    fix: sendmail

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,17 +1,17 @@
 const nodemailer = require("nodemailer");
 
-const sendEmail = async (to, subject, html) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+});
 
+const sendEmail = async (to, subject, html) => {
   await transporter.sendMail({
     from: `"CodeMate" <${process.env.EMAIL_USER}>`,
     to,
